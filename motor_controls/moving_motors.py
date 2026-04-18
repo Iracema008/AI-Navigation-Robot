2ea0065834370dbd97fb62f822f322d77c9d9c01
iracema <iracemac@csu.fullerton.edu>
2026-04-18 06:20:12 +0000

    Added sensor and motor direction

--- motor_controls/moving_motors.py
+++ motor_controls/moving_motors.py
@@ -1,10 +1,18 @@
+
+
 import RPi.GPIO as GPIO
 import motor_controls.gpio_setup as pins
 
-def forward():
-    pass
-def back():
-    pass 
+def forward(speed = 70):
+    GPIO.output(pins.in1, GPIO.LOW)
+    GPIO.output(pins.in2, GPIO.HIGH)
+    GPIO.output(pins.in3, GPIO.HIGH)
+    GPIO.output(pins.in4, GPIO.LOW)
+def stop():
+    GPIO.outout([pins.in1, pins.in2, pins.in3, pins.in4]. GPIO.LOW)
+    pins.pwm_right.ChangeDutyCycle(0)
+    pins.pwm_left.ChangeDutyCycle(0)
+    
 def left():
     pass
 def right():
--- sensors/tcrt_5000.py
+++ sensors/tcrt_5000.py
@@ -1,1 +1,13 @@
 '''Line Sensor Readings'''
+
+import RPi.GPIO as GPIO
+import time
+import motor_controls.gpio_setup as pins
+
+
+
+
+def detect_line():
+    return GPIO.input(pins.line_ir) == GPIO.LOW
+
+
