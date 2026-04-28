fe87531ae765f199a1da07e993eff14dbf367cb2
Iracema008 <Iracemac@csu.fullerton.edu>
2026-04-28 16:34:48 -0700

    forward, stop, left, right config

--- motor_controls/moving_motors.py
+++ motor_controls/moving_motors.py
@@ -3,17 +3,34 @@
 import RPi.GPIO as GPIO
 import motor_controls.gpio_setup as pins
 
-def forward(speed = 70):
+
+def forward():
+    GPIO.output(pins.in1, GPIO.HIGH)
+    GPIO.output(pins.in2, GPIO.LOW)
+    GPIO.output(pins.in3, GPIO.HIGH)
+    GPIO.output(pins.in4, GPIO.LOW)
+
+    pins.pwm_right.ChangeDutyCycle(0)
+    pins.pwm_left.ChangeDutyCycle(0)
+
+# these are the same
+def left(fast=70, slow=35):
+    GPIO.output(pins.in1, GPIO.HIGH)
+    GPIO.output(pins.in2, GPIO.LOW)
+    GPIO.output(pins.in3, GPIO.LOW)
+    GPIO.output(pins.in4, GPIO.HIGH)
+    pins.pwm_right.ChangeDutyCycle(fast)
+    pins.pwm_left.ChangeDutyCycle(slow)
+
+def right(fast=70, slow=35):
     GPIO.output(pins.in1, GPIO.LOW)
     GPIO.output(pins.in2, GPIO.HIGH)
     GPIO.output(pins.in3, GPIO.HIGH)
     GPIO.output(pins.in4, GPIO.LOW)
+    pins.pwm_right.ChangeDutyCycle(slow)
+    pins.pwm_left.ChangeDutyCycle(fast)
+
 def stop():
-    GPIO.outout([pins.in1, pins.in2, pins.in3, pins.in4]. GPIO.LOW)
+    GPIO.output([pins.in1, pins.in2, pins.in3, pins.in4], GPIO.LOW)
     pins.pwm_right.ChangeDutyCycle(0)
     pins.pwm_left.ChangeDutyCycle(0)
-    
-def left():
-    pass
-def right():
-    pass
