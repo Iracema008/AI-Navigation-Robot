47c021c0f1318aaf819ca79d4a127422d19d9917
Iracema008 <Iracemac@csu.fullerton.edu>
2026-04-28 18:58:38 -0700

    line detection

--- sensors/tcrt_5000.py
+++ sensors/tcrt_5000.py
@@ -4,6 +4,8 @@ import RPi.GPIO as GPIO
 import time
 import motor_controls.gpio_setup as pins
 
+from motor_controls.moving_motors import forward, right
+
 
 
 
@@ -11,3 +13,11 @@ def detect_line():
     return GPIO.input(pins.line_ir) == GPIO.LOW
 
 
+def wait_at_intersection(segment_time=2.0):
+    start = time.time()
+    while time.time() - start < segment_time:
+        if detect_line():
+            forward(70)
+        else:
+            right(70, 35)
+        time.sleep(0.05)
